c9bb5fe33ad27c26ca7ce78255d638ce40951e05
Dallin Bland <73759064+SourCreamCulture@users.noreply.github.com>
2025-01-29 17:54:01 -0700

    Initial Commit

---
+++ Prioriti/PrioritiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrioritiApp.swift
+//  Prioriti
+//
+//  Created by Dallin Bland on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PrioritiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
